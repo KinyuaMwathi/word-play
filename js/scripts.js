639aa67ff7e611c303d900f634a8cf4a50e118eb
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-26 14:08:43 +0300

    Add initial form scripts and the jquery library

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form#wordPlayForm").submit(function(event) {
+    event.preventDefault();
+    const sentenceInput = $("input#inputSentence").val();
+
+    const sentenceArray = sentenceInput.split(' ');
+    let newArray = [];
+
+    sentenceArray.forEach(function(element) {
+      return newArray = element.length >= 3;
+    });
+
+    $("#hidden").show();
+    $("#output").text(newArray);
+  });
+});
